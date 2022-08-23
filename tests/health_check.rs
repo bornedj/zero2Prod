7e8a919bddcba2547eb6cb90c09e4daba79a77d7
bornedj <bornedj@miamioh.edu>
2022-08-23 11:04:27 -0400

    added first integration test for health_check endpoint

---
+++ tests/health_check.rs
@@ -0,0 +1,20 @@
+#[tokio::test]
+async fn heaht_check_works() {
+    spawn_app().await.expect("Failed to spawn our app.");
+
+    // Bringing in reqwest to perform HTTP requests against our application
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    zero2prod::run().await
+}

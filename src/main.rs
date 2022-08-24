eb8e3a74adc887575121fd41a86276958dfb17c2
bornedj <bornedj@miamioh.edu>
2022-08-24 14:44:37 -0400

    setup configuration.yaml file to load databse configuration on startup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to random port.");
+    // panic if we cannot get a config file for the database
+    let configuration = get_configuration().expect("Failed to read configuration file");
+    // address coming from config file
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind to random port.");
     run(listener)?.await
 }

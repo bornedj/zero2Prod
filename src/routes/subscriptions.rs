5782c121e2ae61389501fea3cb1621a896d9f7ee
bornedj <bornedj@miamioh.edu>
2022-08-27 10:48:02 -0400

    added logging through actix middleware and log crate

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,16 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // setup logging
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "Request id: '{}' - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+
+    // run insert query
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +33,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request id {} - Failed to execute query {:?}.",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }

676b41c3ee9a5711b7c7892195ae8a0734c7ad29
bornedj <bornedj@miamioh.edu>
2022-08-27 11:37:21 -0400

    added tracing formatting and subscribers to main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // printing all logs at info-level or above,
-    // unless the RUST_LOG environmental
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // unless the RUST_LOG environmental specifies otherwise
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // output the the formatted spans to stdout
+        std::io::stdout,
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("failed to set subscriber");
+
     // panic if we cannot get a config file for the database
     let configuration = get_configuration().expect("Failed to read configuration file");
 

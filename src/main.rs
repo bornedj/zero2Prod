340b08c96563f144ce6308983650e67055424401
bornedj <bornedj@miamioh.edu>
2022-08-27 11:55:48 -0400

    refactored the telemetry logic into a module

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // printing all logs at info-level or above,
-    // unless the RUST_LOG environmental specifies otherwise
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        // output the the formatted spans to stdout
-        std::io::stdout,
-    );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("failed to set subscriber");
-
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     // panic if we cannot get a config file for the database
     let configuration = get_configuration().expect("Failed to read configuration file");
 

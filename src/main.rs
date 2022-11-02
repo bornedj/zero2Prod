fa4f97db569edad8aa424224769fdb153d2dcefe
bornedj <bornedj@miamioh.edu>
2022-11-02 15:04:20 -0400

    updated the configuration.rs file to support environmental changes. Refactored to pgconnectoptions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -15,10 +14,12 @@ async fn main() -> std::io::Result<()> {
     // setting up database connection
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(configuration.database.connection_string().expose_secret())
-        .expect("Failed to create Postgres connection pool");
+        .connect_lazy_with(configuration.database.with_db());
     // address coming from config file
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind to random port.");
     run(listener, connection_pool)?.await
 }

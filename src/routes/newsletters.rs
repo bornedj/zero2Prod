938c4670d759534b3428f1c12a76d47aac7a7225
bornedj <bornedj@miamioh.edu>
2022-12-07 13:46:43 -0500

    added the spec.json file as doctl infrastructure is broken

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -6,7 +6,7 @@ use actix_web::http::{header, StatusCode};
 use actix_web::HttpRequest;
 use actix_web::{web, HttpResponse, ResponseError};
 use anyhow::Context;
-use secrecy::Secret;
+use secrecy::{Secret, ExposeSecret};
 use sqlx::PgPool;
 
 // error handling
@@ -91,7 +91,8 @@ pub async fn publish_newsletter(
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let user_id = validate_credentials(credentials, &pool).await?;
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -152,4 +153,28 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         username,
         password: Secret::new(password),
     })
+}
+
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"
+        SELECT user_id
+        FROM users
+        WHERE username = $1 AND password = $2
+        "#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials.")
+    .map_err(PublishError::AuthError)?;
+
+    user_id
+    .map(|row| row.user_id)
+    .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+    .map_err(PublishError::AuthError)
 }

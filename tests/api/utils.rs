6f3eb759ac8807984d6990654d9501a8dbfccdae
bornedj <bornedj@miamioh.edu>
2022-12-07 12:05:55 -0500

    all tests are green after adding random credentials to post_newsletters

--- tests/api/utils.rs
+++ tests/api/utils.rs
@@ -46,6 +46,8 @@ impl TestApp {
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
+            // added random authentication to make tests happy for now
+            .basic_auth(Uuid::new_v4().to_string(), Some(Uuid::new_v4().to_string()))
             .json(&body)
             .send()
             .await

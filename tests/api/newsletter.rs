80e6bacacb8868f980afbcdc06064e1d1fe52ec2
bornedj <bornedj@miamioh.edu>
2022-12-11 13:35:28 -0500

    added an additional test to ensure non existing users are not sent the newsletter

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,5 @@
 use crate::utils::{spawn_app, ConfirmationLinks, TestApp};
+use uuid::Uuid;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -114,6 +115,36 @@ async fn requests_missing_authorization_are_rejected() {
         .expect("Failed to execute request.");
 
     //assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
+#[tokio::test]
+async fn non_existing_user_is_rejected() {
+    // arrange
+    let app = spawn_app().await;
+    // assign random credentials
+    let username = Uuid::new_v4().to_string();
+    let password = Uuid::new_v4().to_string();
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                   "text": "Newsletter body as plain text",
+                   "html": "<p>Newsletter body as HTML</p>",
+             }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
     assert_eq!(401, response.status().as_u16());
     assert_eq!(
         r#"Basic realm="publish""#,

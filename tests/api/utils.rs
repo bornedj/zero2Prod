64a4fd5760cb30b9036c81f4b023d0755e5759dd
bornedj <bornedj@miamioh.edu>
2022-12-17 13:11:30 -0500

    working on adding cookies for error messaging with login

--- tests/api/utils.rs
+++ tests/api/utils.rs
@@ -55,6 +55,21 @@ impl TestApp {
             .await
             .expect("Failed to execute request")
     }
+
+    pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
+        reqwest::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap()
+            .post(&format!("{}/login", &self.address))
+            .form(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
 }
 
 pub struct TestUser {
@@ -193,4 +208,9 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
 
     // returning the connection pool to the newly made database
     connection_pool
+}
+
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), location);
 }
